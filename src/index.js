2a35a4f7eb0e70ac0f09e7c56daea83b5b1d9a7c
andre ferrer <andrerferrer@hotmail.com>
2021-03-27 07:16:11 -0300

    Finish livecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello from index.js')
+const searchAlgoliaPlaces = (event) => {
+  const url = "https://places-dsn.algolia.net/1/places/query"
+  
+  const queryJson = { query: event.currentTarget.value }
+  
+  // this queryJson.to_s
+  const bodyStrigified = JSON.stringify(queryJson);
+  
+  fetch(url, {
+    method: "POST",
+    body: bodyStrigified
+  })
+    .then(response => response.json())
+    .then((data) => {
+      const city = data.hits[0].locale_names.default[0];
+      console.log(city);
+
+      const result = document.getElementById('result');
+      result.innerText = city;
+    });
+};
+
+const input = document.querySelector("#search");
+input.addEventListener("keyup", searchAlgoliaPlaces);
